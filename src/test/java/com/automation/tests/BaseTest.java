88532824f3e01691a315db641fbd891377563fc1
swetha21188 <swetha21188@gmail.com>
2017-01-13 11:11:43 -0800

    Add files via upload

--- src/test/java/com/automation/tests/BaseTest.java
+++ src/test/java/com/automation/tests/BaseTest.java
@@ -4,6 +4,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
@@ -13,13 +17,30 @@ public class BaseTest {
 @BeforeClass
 	public void openBrowser() throws InterruptedException {
 	
-	 	String driverPath = "/Users/spatoor/Downloads/";
+		//adding the path of the chromedriver into a config file
+		Properties prop = new Properties();
+		InputStream input = null;
+		try {
+			input = new FileInputStream("./resources/config.properties");
+			// load a properties file
+			prop.load(input);
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	 	String driverPath = prop.getProperty("driverPath");
 	 	System.setProperty("webdriver.chrome.driver" , driverPath+"chromedriver");
 	 	driver = new ChromeDriver();
 	 	driver.get("http://store.23andme.com/en-us/");
 	 	driver.manage().window().maximize();
  		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);   
-
   }
 
 @AfterClass
